3f15d90d54608cac7810000bb28049c3e473270d
Bharath Hegde <bhegde8@gmail.com>
2020-05-01 14:29:22 -0400

    Updates

---
+++ precache-manifest.efbe98268bfcb20e95737eb6caba5619.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ac38ad3d01152f0f8712b06274f966",
+    "url": "/omdb-search-app/index.html"
+  },
+  {
+    "revision": "77682fe242a1f08931d5",
+    "url": "/omdb-search-app/static/css/main.a13f1904.chunk.css"
+  },
+  {
+    "revision": "fa7f272e7475e1848818",
+    "url": "/omdb-search-app/static/js/2.b4f75f80.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/omdb-search-app/static/js/2.b4f75f80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77682fe242a1f08931d5",
+    "url": "/omdb-search-app/static/js/main.402bb0aa.chunk.js"
+  },
+  {
+    "revision": "b8063f5e3b99ecf1d1c0",
+    "url": "/omdb-search-app/static/js/runtime-main.224a3964.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/omdb-search-app/static/media/logo.8dad8028.svg"
+  }
+]);
