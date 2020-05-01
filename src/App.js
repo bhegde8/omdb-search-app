debf056855c3aa219345a173b4b0d627f0529eff
Bharath Hegde <bhegde8@gmail.com>
2020-05-01 19:11:21 -0400

    Base layout created

--- src/App.js
+++ src/App.js
@@ -1,56 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './components/counter/Counter';
-import './App.css';
+
+import styles from './App.css';
+
+import SearchBox from './components/searchbox/SearchBox';
+import ResultsGrid from './components/resultsgrid/ResultsGrid';
+
+import { AppBar, CssBaseline, ThemeProvider, Toolbar, Typography } from '@material-ui/core';
+import LocalMoviesIcon from '@material-ui/icons/LocalMovies';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+
+const darkMode = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: red[800]
+    }
+  },
+});
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className={styles.App}>
+      <ThemeProvider theme={darkMode}>
+        
+        <CssBaseline />
+
+        <AppBar position="static">
+          <Toolbar>
+            <LocalMoviesIcon />
+
+            <Typography variant="h5">
+              OMDb Search
+            </Typography>
+
+          </Toolbar>
+        </AppBar>
+
+        <SearchBox />
+
+        <ResultsGrid />
+
+      </ThemeProvider>
+      
     </div>
   );
 }
